7c88eb439294e33b949e0466fe25eeaf0502f66a
刘 先航 <liuxianhgang0505@outlook.com>
2026-07-02 22:20:55 +0800

    改了改吧，喵——

--- Rua/main.cpp
+++ Rua/main.cpp
@@ -7,13 +7,11 @@ int main(){
 #ifdef _DEBUG
 	#pragma message("当前处于DEBUG编译模式，调试功能将开启")
 #endif
-
 	try{
 		初始化窗口();
 		交互与执行();
 	}
 	catch (...){}
-
 	// 如果乱码了，就去【项目属性 → C/C++ → 命令行 → 其他选项】里加一条"/utf-8"试试吧
 	return 0;
 }
--- Rua/工具合集.h
+++ Rua/工具合集.h
@@ -11,17 +11,10 @@ struct 全局 {
 
 extern 全局 全局内容;
 
-// 返回内容的定义
-enum 返回内容 {
-    成功,
-    ab没闭合
-};
-
 // 工具函数的声明
 void 初始化窗口();
 string 获取系统用户名();
 int 生成随机数(const int 左边, const int 右边);
-返回内容 ab匹配(const u32string& 代码_, size_t& 位置_, int& 行位置_, int& 列位置_, const char32_t a边, const char32_t b边, bool 回退 = false);
-u32string ab匹配字符串令牌(const u32string& 代码_, size_t& 位置_, int& 行位置_, int& 列位置_, const char32_t a边, const char32_t b边);
+u32string ab匹配(const u32string& 代码_, size_t& 位置_, int& 行位置_, int& 列位置_, const char32_t a边, const char32_t b边, bool& 成功);
 u32string 读取可能的标识符或关键词令牌(const u32string& 代码_, size_t& 位置_, int& 列位置_);
 bool 可用于标识符字符(char32_t 字符_);
